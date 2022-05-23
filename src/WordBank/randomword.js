d76c114b663476cc5b6d1d0aa26db7365b9452d3
Platy <daryl.limkz@gmail.com>
2022-05-23 19:30:49 +0800

    feat: added more categories
    Signed-off-by: Platy <daryl.limkz@gmail.com>

--- src/WordBank/randomword.js
+++ src/WordBank/randomword.js
@@ -1,11 +1,25 @@
 import aroundthehouse from "./aroundthehouse";
+import beforeandafter from "./beforeandafter";
+import eventt from "./event";
+import fictionalcharacter from "./fictionalcharacter";
+import foodanddrink from "./foodanddrink";
 import funanggames from "./funandgames";
+import inthekitchen from "./inthekitchen";
+import landmark from "./landmark";
+import livingthing from "./livingthing";
 import place from "./place";
 import thing from "./thing";
 
 const wordbank = {
     "Around The House": aroundthehouse,
+    "Before And After": beforeandafter,
+    Event: eventt,
+    "Fictional Character": fictionalcharacter,
+    "Food And Drink": foodanddrink,
     "Fun And Games": funanggames,
+    "In The Kitchen": inthekitchen,
+    Landmark: landmark,
+    "Living Thing": livingthing,
     Place: place,
     Thing: thing,
 };
